7b53dda780f0ee4181f0e344796496ee0e1850af
Ted <tedtenedorio@gmail.com>
2016-06-24 17:13:43 -0700

    Fixed file organization for proper pip package creation

--- ImportDoctor/ImportDoctor/impdoc.py
+++ ImportDoctor/ImportDoctor/impdoc.py
@@ -1,18 +1,54 @@
 import os
 import sys
 import inspect
-import pkgutil
 from collections import defaultdict
+
+py_modules = [
+    u'string', u're', u'difflib', u'textwrap', u'unicodedata', u'stringprep', u'readline', 
+    u'rlcompleter', u'struct', u'codecs', u'datetime', u'calendar', u'collections', 
+    u'collections', u'heapq', u'bisect', u'array', u'weakref', u'types', u'copy', u'pprint', 
+    u'reprlib', u'enum', u'numbers', u'math', u'cmath', u'decimal', u'fractions', u'random', 
+    u'statistics', u'itertools', u'functools', u'operator', u'pathlib', u'os', u'fileinput', 
+    u'stat', u'filecmp', u'tempfile', u'glob', u'fnmatch', u'linecache', u'shutil', u'macpath', 
+    u'pickle', u'copyreg', u'shelve', u'marshal', u'dbm', u'sqlite', u'zlib', u'gzip', u'bz', 
+    u'lzma', u'zipfile', u'tarfile', u'csv', u'configparser', u'netrc', u'xdrlib', u'plistlib', 
+    u'hashlib', u'hmac', u'os', u'io', u'time', u'argparse', u'getopt', u'logging', u'logging', 
+    u'logging', u'getpass', u'curses', u'curses', u'curses', u'curses', u'platform', u'errno', 
+    u'ctypes', u'threading', u'multiprocessing', u'concurrent', u'subprocess', u'sched', u'queue', 
+    u'dummy', u'socket', u'ssl', u'select', u'selectors', u'asyncio', u'asyncore', u'asynchat', 
+    u'signal', u'mmap', u'email', u'json', u'mailcap', u'mailbox', u'mimetypes', u'base', 
+    u'binhex', u'binascii', u'quopri', u'uu', u'html', u'html', u'html', u'xml', u'xml', 
+    u'xml', u'xml', u'xml', u'xml', u'xml', u'xml', u'xml', u'webbrowser', u'cgi', u'cgitb', 
+    u'wsgiref', u'urllib', u'urllib', u'urllib', u'urllib', u'urllib', u'urllib', u'http', 
+    u'http', u'ftplib', u'poplib', u'imaplib', u'nntplib', u'smtplib', u'smtpd', u'telnetlib', 
+    u'uuid', u'socketserver', u'http', u'http', u'http', u'xmlrpc', u'xmlrpc', u'xmlrpc', 
+    u'ipaddress', u'audioop', u'aifc', u'sunau', u'wave', u'chunk', u'colorsys', u'imghdr', 
+    u'sndhdr', u'ossaudiodev', u'gettext', u'locale', u'turtle', u'cmd', u'shlex', u'tkinter', 
+    u'tkinter', u'tkinter', u'tkinter', u'typing', u'pydoc', u'doctest', u'unittest', u'unittest', 
+    u'unittest', u'test', u'test', u'bdb', u'faulthandler', u'pdb', u'timeit', u'trace', 
+    u'tracemalloc', u'distutils', u'ensurepip', u'venv', u'zipapp', u'sys', u'sysconfig', 
+    u'builtins', u'warnings', u'contextlib', u'abc', u'atexit', u'traceback', u'gc', u'inspect', 
+    u'site', u'fpectl', u'code', u'codeop', u'zipimport', u'pkgutil', u'modulefinder', u'runpy', 
+    u'importlib', u'parser', u'ast', u'symtable', u'symbol', u'token', u'keyword', u'tokenize', 
+    u'tabnanny', u'pyclbr', u'py', u'compileall', u'dis', u'pickletools', u'formatter', u'msilib', 
+    u'msvcrt', u'winreg', u'winsound', u'posix', u'pwd', u'spwd', u'grp', u'crypt', u'termios', 
+    u'tty', u'pty', u'fcntl', u'pipes', u'resource', u'nis', u'syslog', u'optparse', u'imp'
+]
 
 # Checks to see if a string matches any of the current system level modules
 def is_sys_module(name):
-    print name, name in sys.modules.keys()
     if name not in sys.modules.keys():
         return False
     pathname = os.path.dirname(inspect.getfile(sys.modules[name]))
     # System modules either come from standard python libraries or from venvs
-    return pathname.startswith('/System/Library/Frameworks') \
-        or pathname.startswith('/Users/tedtenedorio/venvs/lib/python2.7')
+    for path in sys.path:
+        if pathname.startswith(path):
+            return True
+    return False
+    
+def is_py_module(name):
+    return name in py_modules
+
 
 # returns a word with a snippet cut out of it
 # also returns true if the word was changed
@@ -43,7 +79,7 @@ class ImportDoctor(object):
     @classmethod
     def run(cls, filename, **kwargs):
         cls(**kwargs).fix(filename)
-        
+    
     def __vars__(self):
         return set([x for x in dir(self) if not x.startswith('__') 
                     and not callable(getattr(self, x))])
@@ -57,7 +93,7 @@ class ImportDoctor(object):
         with open(filename) as f:
             doc = f.readlines()
         self.source = []
-        self.imports = []
+        self.Q = []
         chars = 0
         state = 0
         
@@ -68,21 +104,21 @@ class ImportDoctor(object):
             if res:
                 x = 1
             return statement, x
-            
+        
         for line in doc:
             if state == 1:
                 focus = line.strip()
                 focus, state = test_backslash(focus)
-                self.imports[-1] += ' ' + focus
+                self.Q[-1] += ' ' + focus
                 continue
-            if state == 2:
+            elif state == 2:
                 focus = line.strip()
                 focus, res = cut_word(focus, ')')
                 if res:
                     state = 0
-                self.imports[-1] += ' ' + focus
+                self.Q[-1] += ' ' + focus
                 continue
-            if self.is_import_statement(line):
+            elif self.is_import_statement(line):
                 focus = line.strip()
                 focus, state = test_backslash(focus)
                 if not state and '(' in focus:
@@ -91,7 +127,7 @@ class ImportDoctor(object):
                     focus, res = cut_word(focus, ')')
                     if res:
                         state = 0
-                self.imports.append(focus)
+                self.Q.append(focus)
                 continue
             self.source.append(line)
             
@@ -100,19 +136,15 @@ class ImportDoctor(object):
             return
         
         store = defaultdict(set)
-        cap = len(self.imports)
+        cap = len(self.Q)
         index = 0
         # analyze each import line
         while index < cap:
             index += 1
-            line = self.imports.pop(0)
+            line = self.Q.pop(0)
             parts = filter(bool, line.split(','))
             base = ' '.join(filter(bool, parts.pop(0).split(' ')))
             
-            # catch single reference and skip splitter
-            if len(parts) == 1:
-                self.imports.append(base)
-                continue
             # create base import, and a list of all imported modules
             base, x = base.rsplit(' ', 1)
             parts.append(x)
@@ -122,10 +154,10 @@ class ImportDoctor(object):
         lam = lambda n: n if self.alpha_order else lambda n: len(n)
         for base, parts in store.iteritems():
             for part in sorted(parts, key=lam):
-                self.imports.append(' '.join([base.strip(), part.strip()]))
+                self.Q.append(' '.join([base.strip(), part.strip()]))
 
     def sort_imports(self):
-        if not self.imports:
+        if not self.Q:
             return
         
         def lamb(n, upto=' import'):
@@ -145,7 +177,7 @@ class ImportDoctor(object):
             lam = lambda n: len(n)
         
         # sort
-        self.Q = sorted(self.imports, key=lam, reverse=self.descending)
+        self.Q = sorted(self.Q, key=lam, reverse=self.descending)
         
         # split on import first
         if self.import_ontop:
@@ -166,15 +198,21 @@ class ImportDoctor(object):
         base = self.Q
         native = []
         index = 0
+        py_native = []
         while index < len(base):
             parts = base[index].split(' ')
             
             if not is_sys_module(parts[1]):
                 index += 1
                 continue
+            if is_py_module(parts[1]):
+                py_native.append(base.pop(index))
+                continue
             native.append(base.pop(index))
         native.append('')
-        self.Q = native
+        py_native.append('')
+        self.Q = py_native
+        self.Q.extend(native)
         self.Q.extend(base)
             
     def remap(self, filename):
@@ -203,12 +241,19 @@ class ImportDoctor(object):
                 f.write(line)
         pass
     
+    def __str__(self):
+        if not self.Q:
+            return 'Uninitialized'
+        rep = ''
+        for line in self.Q:
+            rep += line + '\n'
+        return rep
+    
     def fix(self, filename):
         self.analyze(filename)
         self.sort_imports()
         self.remap(filename)
-
-
+        
 if __name__ == "__main__":
     args = sys.argv
     args.pop(0)
