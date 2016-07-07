336e96ca65ad6d38e45a8fcb5abc196a0f4d9158
Ted <tedtenedorio@gmail.com>
2016-07-06 17:24:28 -0700

    Adding preferences

--- ImportDoctor/ImportDoctor/doctor_base.py
+++ ImportDoctor/ImportDoctor/doctor_base.py
@@ -8,7 +8,7 @@ class ImportNurse(object):
     import_ontop = True         # Place all 'from' imports below standard imports
     wrap_strict = False         # If true, wrap_depth will be strictly enforced
     comment_names = True        # If true, groups will be named in comments above group
-    
+
     # These variables are controlled through getters and setters.
     _alpha_order = False        # Sorts by alphabetical instead of length
     _newline_padding = 1        # How many empty lines after end of imports
@@ -16,13 +16,14 @@ class ImportNurse(object):
     _wrap_depth = 0             # The character count at which line wrapping begins
     _one_import_per_line = True # Separates grouped imports onto their own lines
     _remove_overrides = True    # removes imports that are overridden by others
-    
+
     # groups as follows: from [AModule] import ([A, B, C])
     _from_statement = re.compile('from +([0-9a-zA-Z._]+) +import +\\(?([ 0-9a-zA-Z.,_]+)\\)? *')
     # groups as follows: import ([AModule, BModule])
     _import_statement = re.compile('import +\\(?([ 0-9a-zA-Z.,_]+)\\)? *')
 
     def __init__(self, **kwargs):
+        self.load_prefs()
         self.Q = []
         self.source = []
         if not set(kwargs.keys()) <= self.__vars__():
@@ -32,11 +33,43 @@ class ImportNurse(object):
         self._filter = (lambda n: n) if self._alpha_order else (lambda n: len(n))
         pathpat = re.compile('/python[0-3]\.[0-9.]+/')
         self._sys_paths = [p for p in sys.path if pathpat.search(p)]
-    
+        self.save_prefs()
+
+    def load_prefs(self):
+        pref_file = 'prefs.txt'
+        if not os.path.isfile(pref_file):
+            return
+        with open(pref_file) as f:
+            lines = f.readlines()
+        for line in lines:
+            line = line.strip().split('=')
+            setattr(self, line[0], eval('='.join(line[1:])))
+
+    def save_prefs(self):
+        pref_file = 'prefs.txt'
+        saveable = [
+            'ignore_indented',
+            'group_by_module_type',
+            'descending',
+            'exclude_from',
+            'import_ontop',
+            'wrap_strict',
+            'comment_names',
+            '_alpha_order',
+            '_newline_padding',
+            '_isolation',
+            '_wrap_depth',
+            '_one_import_per_line',
+            '_remove_overrides',
+        ]
+        with open(pref_file, 'w') as f:
+            for var in saveable:
+                f.write('{}={}\n'.format(var, getattr(self, var)))
+
     def __vars__(self):
-        return set([x for x in dir(self) if not x.startswith('__') 
+        return set([x for x in dir(self) if not x.startswith('__')
                     and not callable(getattr(self, x))])
-   
+
     # This function defines a decorator that resets the data parsed if a
     # function decorated with this is called.
     def _purge_state(func):
@@ -46,12 +79,12 @@ class ImportNurse(object):
                 print 'Critical variable ({}) modified. Resetting Queue.'.format(func.__name__)
             self.Q = None
             self.source = None
-        return oncall 
-    
+        return oncall
+
     @property
     def one_import_per_line(self):
         return self._one_import_per_line
-    
+
     @one_import_per_line.setter
     @_purge_state
     def one_import_per_line(self, value):
@@ -59,20 +92,20 @@ class ImportNurse(object):
         if not self._one_import_per_line and self._wrap_depth < 26:
             print 'One import per line has been disabled. Setting default line wrapping at 100 characters'
             self._wrap_depth = 100
-        
+
     @property
     def remove_overrides(self):
         return self._remove_overrides
-    
+
     @remove_overrides.setter
     @_purge_state
     def remove_overrides(self, value):
         self._remove_overrides = bool(value)
-        
+
     @property
     def regex_find_import(self):
         return self._import_statement
-    
+
     @regex_find_import.setter
     @_purge_state
     def regex_find_import(self, value):
@@ -82,11 +115,11 @@ class ImportNurse(object):
             self._import_statement = value
         else:
             raise ValueError('Value must be a string or compiled regex search object.')
-    
+
     @property
     def regex_find_from(self):
         return self._from_statement
-    
+
     @regex_find_from.setter
     @_purge_state
     def regex_find_from(self, value):
@@ -96,22 +129,22 @@ class ImportNurse(object):
             self._from_statement = value
         else:
             raise ValueError('Value must be a string or compiled regex search object.')
-    
+
     @property
     def order_alphabetically(self):
         return self._alpha_order
-    
+
     @order_alphabetically.setter
     @_purge_state
     def order_alphabetically(self, value):
         value = bool(value)
         self._alpha_order = value
         self._filter = (lambda n: n) if value else (lambda n: len(n))
-        
+
     @property
     def wrap_depth(self):
         return self._wrap_depth
-    
+
     @wrap_depth.setter
     def wrap_depth(self, value):
         if type(value) is not int:
@@ -119,11 +152,11 @@ class ImportNurse(object):
         if value < 25:
             raise ValueError('There is no reason to wrap code at 25 characters or less')
         self._wrap_depth = value
-       
+
     @property
     def newline_padding(self):
         return self._newline_padding
-    
+
     @newline_padding.setter
     def newline_padding(self, value):
         if type(value) is not int:
@@ -131,11 +164,11 @@ class ImportNurse(object):
         if value < 0:
             raise ValueError('Padding must be a positive value')
         self._newline_padding = value
-        
+
     @property
     def isolated_groups(self):
         return self._isolation[:]
-    
+
     @isolated_groups.setter
     def isolated_groups(self, value):
         if type(value) is not list:
@@ -177,35 +210,35 @@ def wrap_word(line, depth, strict):
         line = '    ' + line[split_point + 1:]
         split_point = line.find(' ', 5)
     return result + line
-        
+
 builtin_python_modules = [
-    'string', 're', 'difflib', 'textwrap', 'unicodedata', 'stringprep', 'readline', 
-    'rlcompleter', 'struct', 'codecs', 'datetime', 'calendar', 'collections', 
-    'collections', 'heapq', 'bisect', 'array', 'weakref', 'types', 'copy', 'pprint', 
-    'reprlib', 'enum', 'numbers', 'math', 'cmath', 'decimal', 'fractions', 'random', 
-    'statistics', 'itertools', 'functools', 'operator', 'pathlib', 'os', 'fileinput', 
-    'stat', 'filecmp', 'tempfile', 'glob', 'fnmatch', 'linecache', 'shutil', 'macpath', 
-    'pickle', 'copyreg', 'shelve', 'marshal', 'dbm', 'sqlite', 'zlib', 'gzip', 'bz', 
-    'lzma', 'zipfile', 'tarfile', 'csv', 'configparser', 'netrc', 'xdrlib', 'plistlib', 
-    'hashlib', 'hmac', 'os', 'io', 'time', 'argparse', 'getopt', 'logging', 'logging', 
-    'logging', 'getpass', 'curses', 'curses', 'curses', 'curses', 'platform', 'errno', 
-    'ctypes', 'threading', 'multiprocessing', 'concurrent', 'subprocess', 'sched', 'queue', 
-    'dummy', 'socket', 'ssl', 'select', 'selectors', 'asyncio', 'asyncore', 'asynchat', 
-    'signal', 'mmap', 'email', 'json', 'mailcap', 'mailbox', 'mimetypes', 'base', 
-    'binhex', 'binascii', 'quopri', 'uu', 'html', 'html', 'html', 'xml', 'xml', 
-    'xml', 'xml', 'xml', 'xml', 'xml', 'xml', 'xml', 'webbrowser', 'cgi', 'cgitb', 
-    'wsgiref', 'urllib', 'urllib', 'urllib', 'urllib', 'urllib', 'urllib', 'http', 
-    'http', 'ftplib', 'poplib', 'imaplib', 'nntplib', 'smtplib', 'smtpd', 'telnetlib', 
-    'uuid', 'socketserver', 'http', 'http', 'http', 'xmlrpc', 'xmlrpc', 'xmlrpc', 
-    'ipaddress', 'audioop', 'aifc', 'sunau', 'wave', 'chunk', 'colorsys', 'imghdr', 
-    'sndhdr', 'ossaudiodev', 'gettext', 'locale', 'turtle', 'cmd', 'shlex', 'tkinter', 
-    'tkinter', 'tkinter', 'tkinter', 'typing', 'pydoc', 'doctest', 'unittest', 'unittest', 
-    'unittest', 'test', 'test', 'bdb', 'faulthandler', 'pdb', 'timeit', 'trace', 
-    'tracemalloc', 'distutils', 'ensurepip', 'venv', 'zipapp', 'sys', 'sysconfig', 
-    'builtins', 'warnings', 'contextlib', 'abc', 'atexit', 'traceback', 'gc', 'inspect', 
-    'site', 'fpectl', 'code', 'codeop', 'zipimport', 'pkgutil', 'modulefinder', 'runpy', 
-    'importlib', 'parser', 'ast', 'symtable', 'symbol', 'token', 'keyword', 'tokenize', 
-    'tabnanny', 'pyclbr', 'py', 'compileall', 'dis', 'pickletools', 'formatter', 'msilib', 
-    'msvcrt', 'winreg', 'winsound', 'posix', 'pwd', 'spwd', 'grp', 'crypt', 'termios', 
+    'string', 're', 'difflib', 'textwrap', 'unicodedata', 'stringprep', 'readline',
+    'rlcompleter', 'struct', 'codecs', 'datetime', 'calendar', 'collections',
+    'collections', 'heapq', 'bisect', 'array', 'weakref', 'types', 'copy', 'pprint',
+    'reprlib', 'enum', 'numbers', 'math', 'cmath', 'decimal', 'fractions', 'random',
+    'statistics', 'itertools', 'functools', 'operator', 'pathlib', 'os', 'fileinput',
+    'stat', 'filecmp', 'tempfile', 'glob', 'fnmatch', 'linecache', 'shutil', 'macpath',
+    'pickle', 'copyreg', 'shelve', 'marshal', 'dbm', 'sqlite', 'zlib', 'gzip', 'bz',
+    'lzma', 'zipfile', 'tarfile', 'csv', 'configparser', 'netrc', 'xdrlib', 'plistlib',
+    'hashlib', 'hmac', 'os', 'io', 'time', 'argparse', 'getopt', 'logging', 'logging',
+    'logging', 'getpass', 'curses', 'curses', 'curses', 'curses', 'platform', 'errno',
+    'ctypes', 'threading', 'multiprocessing', 'concurrent', 'subprocess', 'sched', 'queue',
+    'dummy', 'socket', 'ssl', 'select', 'selectors', 'asyncio', 'asyncore', 'asynchat',
+    'signal', 'mmap', 'email', 'json', 'mailcap', 'mailbox', 'mimetypes', 'base',
+    'binhex', 'binascii', 'quopri', 'uu', 'html', 'html', 'html', 'xml', 'xml',
+    'xml', 'xml', 'xml', 'xml', 'xml', 'xml', 'xml', 'webbrowser', 'cgi', 'cgitb',
+    'wsgiref', 'urllib', 'urllib', 'urllib', 'urllib', 'urllib', 'urllib', 'http',
+    'http', 'ftplib', 'poplib', 'imaplib', 'nntplib', 'smtplib', 'smtpd', 'telnetlib',
+    'uuid', 'socketserver', 'http', 'http', 'http', 'xmlrpc', 'xmlrpc', 'xmlrpc',
+    'ipaddress', 'audioop', 'aifc', 'sunau', 'wave', 'chunk', 'colorsys', 'imghdr',
+    'sndhdr', 'ossaudiodev', 'gettext', 'locale', 'turtle', 'cmd', 'shlex', 'tkinter',
+    'tkinter', 'tkinter', 'tkinter', 'typing', 'pydoc', 'doctest', 'unittest', 'unittest',
+    'unittest', 'test', 'test', 'bdb', 'faulthandler', 'pdb', 'timeit', 'trace',
+    'tracemalloc', 'distutils', 'ensurepip', 'venv', 'zipapp', 'sys', 'sysconfig',
+    'builtins', 'warnings', 'contextlib', 'abc', 'atexit', 'traceback', 'gc', 'inspect',
+    'site', 'fpectl', 'code', 'codeop', 'zipimport', 'pkgutil', 'modulefinder', 'runpy',
+    'importlib', 'parser', 'ast', 'symtable', 'symbol', 'token', 'keyword', 'tokenize',
+    'tabnanny', 'pyclbr', 'py', 'compileall', 'dis', 'pickletools', 'formatter', 'msilib',
+    'msvcrt', 'winreg', 'winsound', 'posix', 'pwd', 'spwd', 'grp', 'crypt', 'termios',
     'tty', 'pty', 'fcntl', 'pipes', 'resource', 'nis', 'syslog', 'optparse', 'imp'
 ]
